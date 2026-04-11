005d030bee61190b7a1a364d6dc5d595accc5985
shashisahu <shashi.it034@gmail.com>
2026-04-12 02:15:15 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minBuy = Integer.MAX_VALUE;
+        int maxProf = Integer.MIN_VALUE;
+        for(int i =0; i< prices.length; i++){
+            minBuy = Math.min(minBuy, prices[i]);
+            maxProf = Math.max(maxProf , prices[i] - minBuy);
+
+        }
+        return maxProf;
+    }
+}
